3a0a60873a16ec637b1f6474bc81bebb0b62a9b6
/dhamdhawach    Horsuwan <meng.inventor@gmail.com>
2019-05-01 21:38:15 +0700

    v10

---
+++ motion_controller_acc_v10/G0.ino
@@ -0,0 +1,66 @@
+void G0_init(cmd_class& cmd) {
+
+  /*
+    Serial.println("G0 function");
+    Serial.println("W = " + w_str);
+    Serial.println("X = " + x_str);
+    Serial.println("Y = " + y_str);
+    Serial.println("Z = " + z_str);
+  */
+  Serial.print(F("param = "));
+  Serial.println(cmd.str());
+
+  if (cmd.find_param('W').val_available) {
+    float val = cmd.find_param('W').float_val;
+    if (val <= W_MAX&& val >= W_MIN) {
+      motor_w.goal_pos = val* W_STEP_PER_DEG;
+      Serial.println(F("move W"));
+    }
+    else Serial.println(F("ERR,W LIMIT"));
+  }
+  if (cmd.find_param('X').val_available) {
+     float val = cmd.find_param('X').float_val;
+    if (val <= X_MAX && val >= X_MIN) {
+      motor_x.goal_pos = val* X_STEP_PER_DEG;
+      Serial.println(F("move X"));
+    }
+    else Serial.println(F("ERR,X LIMIT"));
+  }
+  if (cmd.find_param('Y').val_available) {
+    float val = cmd.find_param('Y').float_val;
+    if (val <= Y_MAX&& val >= Y_MIN) {
+      motor_y.goal_pos = val* Y_STEP_PER_DEG;
+      Serial.println(F("move Y"));
+    }
+    else Serial.println(F("ERR,Y LIMIT"));
+  }
+  if (cmd.find_param('Z').val_available) {
+    float val = cmd.find_param('Z').float_val;
+    if (val <= Z_MAX && val >= Z_MIN) {
+      motor_z.goal_pos = val* Z_STEP_PER_MM;
+      Serial.println(F("move Z"));
+    }
+    else Serial.println(F("ERR,Z LIMIT"));
+  }
+  if (motor_w.goal_pos == motor_w.crr_pos && motor_x.goal_pos == motor_x.crr_pos && motor_y.goal_pos == motor_y.crr_pos && motor_z.goal_pos == motor_z.crr_pos) {
+    busy_flag = false;
+  }
+  motor_w.init_conacc();
+  motor_x.init_conacc();
+  motor_y.init_conacc();
+  motor_z.init_conacc();
+
+}
+
+void G0_loop(cmd_class& cmd) {
+  if (motor_w.goal_pos == motor_w.crr_pos && motor_x.goal_pos == motor_x.crr_pos && motor_y.goal_pos == motor_y.crr_pos && motor_z.goal_pos == motor_z.crr_pos) {
+
+    busy_flag = false;
+  } else {
+    motor_w.update_conacc();
+    motor_x.update_conacc();
+    motor_y.update_conacc();
+    motor_z.update_conacc();
+  }
+
+}
---
+++ motion_controller_acc_v10/M_CODE.ino
@@ -0,0 +1,57 @@
+void M114(){
+  Serial.print("MOTOR-X ");
+  Serial.println(motor_x.crr_pos/X_STEP_PER_DEG);
+  Serial.print("MOTOR-Y ");
+  Serial.println(motor_y.crr_pos/Y_STEP_PER_DEG);
+  Serial.print("MOTOR-Z ");
+  Serial.println(motor_z.crr_pos/Z_STEP_PER_MM);
+  Serial.print("MOTOR-W ");
+  Serial.println(motor_w.crr_pos/W_STEP_PER_DEG);
+}
+void M119() {
+
+  Serial.print("Z-ENDSTOP : ");
+  Serial.println(digitalRead(ENDSTOP_PIN));
+  Serial.print("MOTOR-X : ");
+  float val = 0;
+  for (int i = 0; i < 100; i++) {
+    val += read_ADC(MOTOR_X_ADC);
+  }
+  val = val / 100;
+  val = float_map(val,ADC_X_MAX,ADC_X_MIN,90,-90);
+  Serial.println(val);
+  Serial.print("MOTOR-Y : ");
+  val = 0;
+  for (int i = 0; i < 100; i++) {
+    val += read_ADC(MOTOR_Y_ADC);
+  }
+  val = val / 100;
+  val = float_map(val,ADC_Y_MAX,ADC_Y_MIN,90,-90);
+  Serial.println(val);
+  Serial.print("MOTOR-W : ");
+  val = 0;
+  for (int i = 0; i < 100; i++) {
+    val += read_ADC(MOTOR_W_ADC);
+  }
+  val = val / 100;
+  val = float_map(val,ADC_W_MAX,ADC_W_MIN,90,-90);
+  Serial.println(val);
+  Serial.print("GRIPPER-SENSOR : ");
+  val = 0;
+  for (int i = 0; i < 100; i++) {
+    val += read_ADC(GRIPPER_ADC);
+  }
+  val = val / 100;
+  Serial.println(val);
+
+
+}
+void M5() { //stop spindle
+  digitalWrite(SOLENOID_PIN, LOW);
+}
+void M3() { //turn on spindle
+  digitalWrite(SOLENOID_PIN, HIGH);
+}
+void M4() { //turn on spindle
+  M3();
+}
